d4cdb2cc7088bb78305eb2be92dc34778743a460
Samuel G. Torres <60748162+SamuelGTL@users.noreply.github.com>
2023-11-28 04:19:58 +0000

    Upload the game logic

--- app.py
+++ app.py
@@ -9,3 +9,75 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+#print("--Paper, Rock, Scissors Game--!")
+#create a loop that repeat the actions till the user wants to stop
+win = 0
+games = 0;
+while True:
+    games += 1;
+    #import the random module
+    import random
+    #create a list of play options
+    t = ["Rock", "Paper", "Scissors"]
+    #assign a random play to the computer
+    computer = t[random.randint(0,2)]
+    #set player to False
+    player = False
+    while player == False:
+        #set player to True
+        player = input("Rock, Paper, Scissors?")
+        if player == computer:
+            print("-----------------------------------")
+            #show the variable seleced by the computer
+            print("Computer selected: ", computer)
+            print("Tie!")
+        elif player == "Rock":
+            if computer == "Paper":
+                print("-----------------------------------")
+                #show the variable seleced by the computer
+                print("Computer selected: ", computer)
+                print("You lose!", computer, "covers", player)
+            else:
+                print("-----------------------------------")
+                #show the variable seleced by the computer
+                print("Computer selected: ", computer)
+                print("You win!", player, "smashes", computer)
+                win += 1
+        elif player == "Paper":
+            if computer == "Scissors":
+                print("-----------------------------------")
+                #show the variable seleced by the computer
+                print("Computer selected: ", computer)
+                print("You lose!", computer, "cut", player)
+            else:
+                print("-----------------------------------")
+                #show the variable seleced by the computer
+                print("Computer selected: ", computer)
+                print("You win!", player, "covers", computer)
+                win += 1
+        elif player == "Scissors":
+            if computer == "Rock":
+                print("-----------------------------------")
+                #show the variable seleced by the computer
+                print("Computer selected: ", computer)
+                print("You lose...", computer, "smashes", player)
+            else:
+                print("-----------------------------------")
+                print("Computer selected: ", computer)
+                print("You win!", player, "cut", computer)
+                win += 1
+        else:
+            print("That's not a valid play. Check your spelling!")
+        #player was set to True, but we want it to be False so the loop continues
+        #player = False
+        computer = t[random.randint(0,2)]
+    #ask the user if they want to play again
+    play_again = input("Do you want to play again? (Y/N)")
+    if play_again.upper() != "Y":
+        #show the count of games winned by the user
+        print("Games played: ", games)
+        print("You win", win, "games!")
+        break
+
+
